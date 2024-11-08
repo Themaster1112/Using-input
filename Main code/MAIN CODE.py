d67d6af03182b2490f8ab1ed89ab56ad714a390d
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-08 16:45:36 +0000

    MAIN code

---
+++ Main code/MAIN CODE.py
@@ -0,0 +1,9 @@
+#Ahh yes input basically its a question that someone answers here are some examples.
+input("How many footballs do you have?")
+print("Okay the school board just needed to know.")
+
+input("How many footballs do you have?")
+if input == "0":
+    print("Please buy some")
+else:
+    print("You have enough")
